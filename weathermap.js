1422bae2e8156cf6ea3a19ac22df50c423fc552a
Ileen Fan <ileenf@Ileens-MacBook-Pro.local>
2019-07-12 12:52:48 -0700

    added south africa pan

---
+++ weathermap.js
@@ -0,0 +1,42 @@
+var view;
+var map;
+var initialLocation;
+var london;
+
+function init() {
+  initialLocation = ol.proj.fromLonLat([41.043316, 28.862457])
+  london = ol.proj.fromLonLat([-0.12755, 51.507222])
+  southafrica = ol.proj.fromLonLat([-30.312099, 23.010606])
+
+  view = new ol.View({
+  center: initialLocation,
+  zoom: 6
+});
+
+  map = new ol.Map({
+       target: 'map',
+       layers: [
+         new ol.layer.Tile({
+           source: new ol.source.OSM()
+         })
+       ],
+       loadTilesWhileAnimating: true,
+       view: view
+     });
+}
+
+function panHome() {
+  view.animate({
+    center: london,
+    duration: 2000
+  })
+}
+
+function panSouthAfrica() {
+  view.animate({
+    center: southafrica,
+    duration: 2000
+  })
+}
+
+window.onload = init;
