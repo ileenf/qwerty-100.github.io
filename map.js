843ce27ea4f466b204ab7afba3b58238d5272301
Ileen Fan <ileenf@Ileens-MacBook-Pro.local>
2019-07-12 12:56:21 -0700

    made map.html to a js file

---
+++ map.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+    <style>
+      .map {
+        height: 400px;
+        width: 100%;
+      }
+    </style>
+    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+    <script src="weathermap.js"></script>
+
+    <title>Weather Map</title>
+
+  </head>
+  <body>
+
+    <p>
+      <button onclick="panHome()">Pan back to London</button>
+      <button onclick="panSouthAfrica()">Pan to South Africa</button>
+
+    </p>
+    <div id="map" class="map"></div>
+
+  </body>
+
+
+</html>
